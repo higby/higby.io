ee8db63ae7e2854014be136b9fcbc26cb964f94f
Branden Higby <branden@higby.io>
2021-08-15 15:36:56 -0700

    Testing

--- src/config/plugins/src/screenshot.js
+++ src/config/plugins/src/screenshot.js
@@ -6,20 +6,18 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addLinter("screenshots", async function(content, outputPath) {
 
     const dom = new JSDOM(content);
-    const link = [...dom.window.document.querySelector("link[rel='canonical']").href];
-
-    console.log(link);
+    const link = dom.window.document.querySelector("body").getAttribute('data-attribute');
 
     if (!fs.existsSync("build/assets/images/opengraph")){
       fs.mkdirSync("build/assets/images/opengraph");
     }
 
-    if( outputPath && outputPath.endsWith(".html") ) {
+    if( link && outputPath.endsWith(".md") ) {
       const browser = await puppeteer.launch({defaultViewport: null});
       const page = await browser.newPage();
       await page.setViewport({ width: 1200, height: 600 });
       await page.setContent(content);
-      await page.screenshot({ path: 'build/assets/images/opengraph/' + "test" + '.png' });
+      await page.screenshot({ path: 'build/assets/images/opengraph/' + link + '.png' });
       await browser.close();
     }
   });
