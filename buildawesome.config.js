7e08a52e09ad257dfdd0faf709131c2d3a27cd95
Branden Higby <b@higby.io>
2026-09-08 23:10:35 -0700

    Add CSS extension

--- buildawesome.config.js
+++ buildawesome.config.js
@@ -1,9 +1,22 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img"
+import { bundle, Features } from "lightningcss"
 
 export default async function ($config) {
 	$config.setInputDirectory("source")
 	$config.addPassthroughCopy({ "source/_static/": "/" })
 	$config.ignores.add("source/_static/")
+
+	$config.addExtension("css", {
+		compile: async (_, inputPath) => async () =>
+			bundle({
+				filename: inputPath,
+				minify: true,
+				exclude: Features.DirSelector | Features.LightDark
+			}).code,
+		outputFileExtension: "css",
+		useLayouts: false
+	})
+	$config.addTemplateFormats("css")
 
 	$config.addPlugin(eleventyImageTransformPlugin, {
 		formats: ["avif", "webp", "jpeg"],
