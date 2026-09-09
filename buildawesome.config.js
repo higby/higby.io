2ad8617d8a6456667939e744caf27d6f074010ba
Branden Higby <b@higby.io>
2026-09-08 23:07:00 -0700

    Add Eleventy-img

--- buildawesome.config.js
+++ buildawesome.config.js
@@ -1,5 +1,12 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img"
+
 export default async function ($config) {
 	$config.setInputDirectory("source")
 	$config.addPassthroughCopy({ "source/_static/": "/" })
 	$config.ignores.add("source/_static/")
+
+	$config.addPlugin(eleventyImageTransformPlugin, {
+		formats: ["avif", "webp", "jpeg"],
+		transformOnRequest: false
+	})
 }
