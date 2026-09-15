6973f7578022ac4f01ccfc48f0ea4ac86c4f1791
Branden Higby <b@higby.io>
2026-09-14 22:45:11 -0700

    Move reusable config to @higby/eleventy-config

--- buildawesome.config.js
+++ buildawesome.config.js
@@ -1,31 +1,13 @@
 import { EleventyHtmlBasePlugin, IdAttributePlugin } from "@11ty/eleventy"
-import { eleventyImageTransformPlugin } from "@11ty/eleventy-img"
+import config from "@higby/eleventy-config"
 import { VentoPlugin } from "eleventy-plugin-vento"
-import { bundle, Features } from "lightningcss"
 
 export default async function ($config) {
 	$config.setInputDirectory("source")
 	$config.addPassthroughCopy({ "source/_static/": "/" })
 	$config.ignores.add("source/_static/")
-	$config.setDataFileSuffixes([""])
 
-	$config.addExtension("css", {
-		compile: async (_, inputPath) => async () =>
-			bundle({
-				filename: inputPath,
-				minify: true,
-				exclude: Features.DirSelector | Features.LightDark
-			}).code,
-		outputFileExtension: "css",
-		useLayouts: false
-	})
-	$config.addTemplateFormats("css")
-
-	$config.addPlugin(eleventyImageTransformPlugin, {
-		formats: ["avif", "webp", "jpeg"],
-		transformOnRequest: false
-	})
-
+	$config.addPlugin(config)
 	$config.addPlugin(EleventyHtmlBasePlugin)
 	$config.addPlugin(IdAttributePlugin)
 	$config.addPlugin(VentoPlugin)
